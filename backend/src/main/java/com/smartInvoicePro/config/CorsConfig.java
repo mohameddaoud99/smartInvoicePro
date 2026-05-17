88e94c962c368178b5ef7617240479124335ee5d
Mohamed Tijani Daoud <mohamed.tijani.daoud@gmail.com>
2026-05-17 15:38:23 +0100

    setup supabase

--- backend/src/main/java/com/smartInvoicePro/config/CorsConfig.java
+++ backend/src/main/java/com/smartInvoicePro/config/CorsConfig.java
@@ -16,7 +16,8 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:4200",
-                "http://localhost"
+                "http://localhost",
+                "https://smartInvoicePro.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
